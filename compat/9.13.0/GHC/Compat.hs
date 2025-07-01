f8a04e871ed9ba5f75a64a08aa5ff7cf5221dfb2
Tobias Dammers <tdammers@gmail.com>
2025-07-01 12:22:45 +0200

    Add support for GHC 9.12.2

---
+++ compat/9.13.0/GHC/Compat.hs
@@ -0,0 +1,19 @@
+-- GHC 9.12.1 compatibility
+module GHC.Compat
+  ( GHC.mkNamePprCtxForModule
+  , mkShowSub
+  ) where
+
+import GHC (ModuleInfo)
+import GHC qualified
+import GHC.Iface.Syntax (AltPpr (..), ShowForAllFlag (..), ShowHowMuch (..), ShowSub (..))
+
+import PrintApi.IgnoredDeclarations
+
+mkShowSub :: ModuleInfo -> ShowSub
+mkShowSub mod_info =
+  let ss_how_much = ShowSome (Just (showOcc mod_info)) (AltPpr Nothing)
+   in ShowSub
+        { ss_how_much = ss_how_much
+        , ss_forall = ShowForAllMust
+        }
